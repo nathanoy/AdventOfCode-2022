92fae217a31d63b0ea530d182596d8f76f749d14
nathanoy <nathanoy@gmx.de>
2022-12-08 16:45:00 +0100

    played a bit

---
+++ day8/playground.py
@@ -0,0 +1,98 @@
+from PIL import Image
+import numpy as np
+
+FILE = "input.txt"
+
+
+def part1():
+    data_ = open(FILE, "r").read().strip()
+    data = []
+    vis_ma = []
+    for line in data_.splitlines():
+        x = list(line)
+        data.append(list(map(int, list(line))))
+        vis_ma.append([False] * len(x))
+
+    for direction in range(4):
+        vis_ma = list(map(list, zip(*vis_ma)))[::-1]
+        # rotate it, algorithm works from top to bottom, when rotated all 4 sides considered
+        data = list(map(list, zip(*data)))[::-1]
+
+        for idx, row in enumerate(data):
+            for i, cell in enumerate(row):
+                m = -1  # m: maximum, if tree is higher => visible
+                if idx > 0:  # first row is always visible
+                    # check all trees above the current one, and remember the highest height: m
+                    for x in data[:idx]:
+                        m = max(x[i], m)
+                        if m == 9: break  # just a little more performant, as no tree is higher than 9
+                if m < cell:  # current tree visible, as all above smaller
+                    vis_ma[idx][i] = True  # set the current one to visible
+
+    answer = sum(map(sum, vis_ma))
+
+    array = np.empty((len(vis_ma), len(vis_ma[0]), 3))
+    for idx, row in enumerate(vis_ma):
+        for i, cell in enumerate(row):
+            row[i] = (255, 255, 255) if cell else (0, 0, 0)
+        array[idx] = np.array(row)
+    Image.fromarray(array.astype("uint8"), "RGB").save("visibility_map.png")
+
+
+def part2():
+    data_ = open(FILE, "r").read().strip()
+    data = []
+    score_map = []
+    for line in data_.splitlines():
+        x = list(line)
+        data.append(list(map(int, list(line))))
+        score_map.append([0] * len(x))
+
+    for i in range(len(data)):
+        for j in range(len(data[0])):
+            x = data[i][j]
+            row = data[i]
+            col = [a[j] for a in data]
+            for rng in (col[:i][::-1], row[:j][::-1], col[i + 1:], row[j + 1:],):
+
+                if len(rng) == 0:
+                    score_map[i][j] = 0
+                    break
+                d = 1
+                for k in rng:
+                    if k >= x:
+                        break
+                    d += 1
+                d = min(d, len(rng))
+                score_map[i][j] = d * max(score_map[i][j], 1)
+
+    m = max(map(max, score_map))
+
+    array = np.empty((len(score_map), len(score_map[0]), 3))
+    for idx, row in enumerate(score_map):
+        for i, cell in enumerate(row):
+            z = 0 if cell == 0 else round(m / cell)
+            row[i] = (z, z, z) if cell != m else (255, 0, 0)
+        array[idx] = np.array(row)
+    Image.fromarray(array.astype("uint8"), "RGB").save("score_map.png")
+
+    score_map = data
+
+    m = max(map(max, score_map))
+
+    array = np.empty((len(score_map), len(score_map[0]), 3))
+    for idx, row in enumerate(score_map):
+        for i, cell in enumerate(row):
+            z = 0 if cell == 0 else round(m / cell)
+            row[i] = (z, z, z) if cell != m else (255, 0, 0)
+        array[idx] = np.array(row)
+    Image.fromarray(array.astype("uint8"), "RGB").save("height_map.png")
+
+
+def solution():
+    part1()
+    part2()
+
+
+if __name__ == '__main__':
+    solution()
--- day8/solution.py
+++ day8/solution.py
@@ -1,60 +1,49 @@
-import pprint
-
 FILE = "input.txt"
 
 
 def part1():
     data_ = open(FILE, "r").read().strip()
-    # print(repr(data))
     data = []
     vis_ma = []
     for line in data_.splitlines():
         x = list(line)
         data.append(list(map(int, list(line))))
         vis_ma.append([False] * len(x))
-    # pprint.pprint(data)
-    # pprint.pprint(vis_ma)
+
     for direction in range(4):
         vis_ma = list(map(list, zip(*vis_ma)))[::-1]
+        # rotate it, algorithm works from top to bottom, when rotated all 4 sides considered
         data = list(map(list, zip(*data)))[::-1]
-        for idx, row in enumerate(data, start=0):
-            # print(row, data[:idx])
-            for i, col in enumerate(row):
-                m = -1
-                if idx > 0:
+
+        for idx, row in enumerate(data):
+            for i, cell in enumerate(row):
+                m = -1  # m: maximum, if tree is higher => visible
+                if idx > 0:  # first row is always visible
+                    # check all trees above the current one, and remember the highest height: m
                     for x in data[:idx]:
                         m = max(x[i], m)
+                        if m == 9: break  # just a little more performant, as no tree is higher than 9
+                if m < cell:  # current tree visible, as all above smaller
+                    vis_ma[idx][i] = True  # set the current one to visible
 
-                if m < col:
-                    vis_ma[idx][i] = True
-
-    # pprint.pprint(vis_ma)
-    c = 0
-    for x in vis_ma:
-        c += sum(x)
-    print(c)
+    print(sum(map(sum, vis_ma)))
 
 
 def part2():
     data_ = open(FILE, "r").read().strip()
-    # print(repr(data))
     data = []
     score_map = []
     for line in data_.splitlines():
         x = list(line)
         data.append(list(map(int, list(line))))
         score_map.append([0] * len(x))
-    # pprint.pprint(data)
-    # pprint.pprint(score_map)
+
     for i in range(len(data)):
         for j in range(len(data[0])):
             x = data[i][j]
             row = data[i]
             col = [a[j] for a in data]
-            rngs = (col[:i][::-1], row[:j][::-1], col[i + 1:], row[j + 1:],)
-            # print(i, j, x, rngs)
-
-            for rng in rngs:
+            for rng in (col[:i][::-1], row[:j][::-1], col[i + 1:], row[j + 1:],):
 
                 if len(rng) == 0:
                     score_map[i][j] = 0
@@ -66,25 +55,7 @@ def part2():
                     d += 1
                 d = min(d, len(rng))
                 score_map[i][j] = d * max(score_map[i][j], 1)
-                # print("   ", d, score_map[i][j], rng)
 
-        #     print()
-        #     break
-        # break
-        # s = 1
-        # m = -1
-        # for a in row[j + 1:]:
-        #     print(a, m, x, a > x, m >= a)
-        #     if a > x or m >= a:
-        #         break
-        #
-        #     m = max(m, a)
-        #     s *= a
-        # if s == 1: s = 0
-        # print(row, col, row[j + 1:], x, s)
-        # score_map[i][j] = s
-
-    # pprint.pprint(score_map)
     print(max(map(max, score_map)))
 
 
